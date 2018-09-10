8bb46bc7554603189b38aab02817a6197ba0daaa
Student <student@lan.itstep.kiev.ua>
2018-09-10 21:53:32 +0300

    10092018 21:53

---
+++ CommandsStart/Commands/MyCommands.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CommandsStart.Commands
+{
+    class MyCommands
+    {
+        public static RoutedCommand Exit { get; set; }
+
+        public static RoutedCommand Reset { get; set; }
+
+        public static RoutedCommand Help { get; set; }
+
+        static MyCommands()
+        {
+            Exit = new RoutedCommand("Exit", typeof(MyCommands));
+            Reset = new RoutedCommand("Reset", typeof(MyCommands));
+            Help = new RoutedCommand("Help", typeof(MyCommands));
+        }
+    }
+}
--- CommandsStart/MainWindow.xaml.cs
+++ CommandsStart/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
 
 namespace CommandsStart
 {
@@ -23,6 +25,46 @@ namespace CommandsStart
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void NewCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+
+        private void OpenCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if(ofd.ShowDialog() == true)
+            {
+                string path = ofd.FileName;
+                text.Text = File.ReadAllText(path);
+            }
+        }
+
+        private void SaveCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            SaveFileDialog ofd = new SaveFileDialog();
+            if (ofd.ShowDialog() == true)
+            {
+                string path = ofd.FileName;
+                File.WriteAllText(path, text.Text);
+            }
+        }
+
+        private void ExitCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ResetCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            text.Clear();
+        }
+
+        private void HelpCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("HURRAY, MAN!");
         }
     }
 }
